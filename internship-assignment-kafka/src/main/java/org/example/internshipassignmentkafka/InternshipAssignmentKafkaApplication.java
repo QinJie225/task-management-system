3c02cc2e6ea7e9b078d5ee3f409c72f6ae61d30f
qfoong <Qin-Jie.Foong@endava.com>
2026-03-16 15:43:33 +0800

    Amend Code - Submit screenshots of MongoDB data - Submit screenshots of logs showing Kafka producer and consumer working - Submit API testing collection

---
+++ internship-assignment-kafka/src/main/java/org/example/internshipassignmentkafka/InternshipAssignmentKafkaApplication.java
@@ -0,0 +1,17 @@
+package org.example.internshipassignmentkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableMongoAuditing
+public class InternshipAssignmentKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipAssignmentKafkaApplication.class, args);
+    }
+
+}
