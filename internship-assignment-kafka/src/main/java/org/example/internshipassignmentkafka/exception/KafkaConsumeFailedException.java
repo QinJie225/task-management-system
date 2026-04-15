dc3e5ebacd17526287cf5371529d5748ff726c54
qfoong <Qin-Jie.Foong@endava.com>
2026-04-15 10:09:52 +0800

    Amend code - Amend the Commit Async - Amend KafkaConsumerFailedException - Refactor the flow of taskEventConsumer on validating the events and handling the errors

--- internship-assignment-kafka/src/main/java/org/example/internshipassignmentkafka/exception/KafkaConsumeFailedException.java
+++ internship-assignment-kafka/src/main/java/org/example/internshipassignmentkafka/exception/KafkaConsumeFailedException.java
@@ -1,8 +1,16 @@
 package org.example.internshipassignmentkafka.exception;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class KafkaConsumeFailedException extends RuntimeException {
-    public KafkaConsumeFailedException(String eventType, String taskId, Throwable cause) {
+    private final List<String> errors;
+
+    public KafkaConsumeFailedException(String eventType, List<String> errors, Throwable cause) {
         super("Failed to consume Kafka event: " + eventType +
-                (taskId != null ? " for taskId=" + taskId : ""), cause);
+                (errors != null && !errors.isEmpty() ? " - reasons: " + errors : ""), cause);
+        this.errors = errors != null ? errors : List.of();
     }
 }
